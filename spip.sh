2e375a05f2f62db394b387641773f1534421fb66
Viktor Ekholm <vieekk@gmail.com>
2016-12-11 00:54:59 +0100

    Refactoring

--- spip.sh
+++ spip.sh
@@ -1,41 +1,13 @@
 #!/bin/bash
 
-# google-chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list"
-
-# dropbox
-sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
-sudo add-apt-repository -y "deb http://linux.dropbox.com/ubuntu xenial main"
-
-# spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-sudo add-apt-repository -y "deb http://repository.spotify.com stable non-free"
-
-# sublime-text-3
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-
-# apt-get
+sudo ./sources.sh
 packages="$(xargs printf ' %s' < packages | cut -b 2-)"
 sudo apt-get update && sudo apt-get install -y $packages
 
-# composer
-mkdir ~/bin
-EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
-
-if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]
-then
-    php composer-setup.php --install-dir=bin --filename=composer --quiet
-    RESULT=$?
-    rm composer-setup.php
-    exit $RESULT
-else
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
+if [-x "$(command -v php)"]; then
+    sudo ./composer.sh
 fi
 
-# npm
-sudo npm install -g bower
+if [-x "$(command -v npm)"]; then
+    sudo ./npm.sh
+fi
