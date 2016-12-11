0cff98cff80dceab805e5896a37ff313219c4eae
Viktor Ekholm <vieekk@gmail.com>
2016-12-11 12:40:24 +0100

    Google Chrome

---
+++ sources/google-chrome.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list"
