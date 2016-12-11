a68db5feab9cd3783e34b04d00c5e470f25e291c
Viktor Ekholm <vieekk@gmail.com>
2016-12-11 01:51:21 +0100

    Using command

--- spip.sh
+++ spip.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-sudo chmod +x sources.sh
+sudo chmod +x sources.shsu
 sudo ./sources.sh
 packages="$(xargs printf ' %s' < packages | cut -b 2-)"
 sudo apt-get update && sudo apt-get install -y $packages
 
-if [-x "$(command -v php)"]; then
-    sudo chmod +x composer.sh
+if command -v php >/dev/null 2>&1; then
     sudo ./composer.sh
 fi
 
-if [-x "$(command -v npm)"]; then
-    sudo chmod +x npm.sh
+if command -v npm >/dev/null 2>&1; then
     sudo ./npm.sh
 fi
