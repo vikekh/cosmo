ed3f461c8781dabaf4c31637362ffcd72103f537
Viktor Ekholm <vieekk@gmail.com>
2016-12-11 12:53:44 +0100

    Loop over and execute subdirectory scripts

--- spip.sh
+++ spip.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-sudo chmod +x sources.sh composer.sh npm.sh
-sudo ./sources.sh
+for file in /sources/*.sh
+do
+    sudo chmod +x $file
+    sudo "./$file"
+done
+
 packages="$(xargs printf ' %s' < packages | cut -b 2-)"
 sudo apt-get update && sudo apt-get install -y $packages
 
-if command -v php >/dev/null 2>&1; then
-    sudo ./composer.sh
+if command -v php >/dev/null 2>&1
+then
+    for file in /post/php*.sh
+    do
+        sudo chmod +x $file
+        sudo "./$file"
+    done
 fi
 
-if command -v npm >/dev/null 2>&1; then
-    sudo ./npm.sh
+if command -v npm >/dev/null 2>&1
+then
+    for file in /post/npm*.sh
+    do
+        sudo chmod +x $file
+        sudo "./$file"
+    done
 fi
