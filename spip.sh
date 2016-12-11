aebb361e199d476f9ef89cdadf4e7f06458495a5
Viktor Ekholm <vieekk@gmail.com>
2016-12-11 01:26:41 +0100

    Make scripts executable

--- spip.sh
+++ spip.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+sudo chmod +x sources.sh
 sudo ./sources.sh
 packages="$(xargs printf ' %s' < packages | cut -b 2-)"
 sudo apt-get update && sudo apt-get install -y $packages
 
 if [-x "$(command -v php)"]; then
+    sudo chmod +x composer.sh
     sudo ./composer.sh
 fi
 
 if [-x "$(command -v npm)"]; then
+    sudo chmod +x npm.sh
     sudo ./npm.sh
 fi
