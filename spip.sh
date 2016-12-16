8b15bf4672e198d58d804b1f006d45df56f0f8d5
Viktor Ekholm <vieekk@gmail.com>
2016-12-16 20:39:40 +0100

    Added support for git scripts

--- spip.sh
+++ spip.sh
@@ -41,34 +41,49 @@ fi
 sudo apt-get update && sudo apt-get install -y $packages
 spip-echo "Done installing packages."
 
-if command -v php >/dev/null 2>&1
+if command -v git >/dev/null 2>&1
 then
-    spip-echo "Installing php packages..."
+    spip-echo "Running git scripts..."
     
-    for file in post/php*.sh
+    for file in post/git*.sh
     do
         sudo chmod +x $file
-        sudo "./$file"
+        ./$file
     done
     
-    spip-echo "Done installing php packages."
+    spip-echo "Done running git scripts."
 else
-    spip-echo "php is not installed."
+    spip-echo "git is not installed."
 fi
 
 if command -v npm >/dev/null 2>&1
 then
-    spip-echo "Installing npm packages..."
+    spip-echo "Running npm scripts..."
     
     for file in post/npm*.sh
     do
         sudo chmod +x $file
-        sudo "./$file"
+        ./$file
     done
     
-    spip-echo "Done installing npm packages."
+    spip-echo "Done running npm scripts."
 else
     spip-echo "npm is not installed."
+fi
+
+if command -v php >/dev/null 2>&1
+then
+    spip-echo "Running php scripts..."
+    
+    for file in post/php*.sh
+    do
+        sudo chmod +x $file
+        ./$file
+    done
+    
+    spip-echo "Done running php scripts."
+else
+    spip-echo "php is not installed."
 fi
 
 spip-echo "Bye bye!"
