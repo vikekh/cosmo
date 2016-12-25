f4d836241993ccda5f8e5a7655071186693d851b
Viktor Ekholm <vieekk@gmail.com>
2016-12-25 22:08:19 +0100

    Not a valid identifier...

--- post/git.sh
+++ post/git.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-spip-echo "git email:"
-read git-email
-spip-echo "git name:"
-read git-name
-ssh-keygen -t rsa -b 4096 -C "$git-email" -f ~/.ssh/id_rsa -N ""
+spip-echo "git demands your e-mail address:"
+read git_email
+spip-echo "git demands your name:"
+read git_name
+ssh-keygen -t rsa -b 4096 -C "$git_email" -f ~/.ssh/id_rsa -N ""
 git config --global core.editor vim
-git config --global user.email "$git-email"
-git config --global user.name "$git-name"
+git config --global user.email "$git_email"
+git config --global user.name "$git_name"
