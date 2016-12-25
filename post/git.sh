057d35d2cf70e4a93c4ba096e078caf8313f4889
Viktor Ekholm <vieekk@gmail.com>
2016-12-25 23:17:56 +0100

    Create .ssh directory

--- post/git.sh
+++ post/git.sh
@@ -5,6 +5,7 @@ spip-echo "git_email:"
 read git_email
 spip-echo "git_name:"
 read git_name
+mkdir ~/.ssh
 ssh-keygen -t rsa -b 4096 -C "$git_email" -f ~/.ssh/id_rsa -N ""
 git config --global core.editor vim
 git config --global user.email "$git_email"
