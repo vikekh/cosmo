880fe240bfbf2bd9061c53e43f01c5f800b545bc
Viktor Ekholm <vieekk@gmail.com>
2017-03-22 22:49:35 +0100

    Post scripts less strict on installed apps

--- spip.sh
+++ spip.sh
@@ -2,6 +2,9 @@
 
 . common.sh
 version=$(<version)
+release="$(lsb_release -i -s) $(lsb_release -r -s)"
+release=${release,,}
+release=${release// /-}
 spip-echo "Hello! This is spip v$version."
 spip-echo "Adding sources..."
 
@@ -12,9 +15,6 @@ do
 done
 
 spip-echo "Done adding sources."
-release="$(lsb_release -i -s) $(lsb_release -r -s)"
-release=${release,,}
-release=${release// /-}
 spip-echo "Adding $release sources..."
 
 if [ -d "sources/$release" ]
@@ -38,50 +38,25 @@ fi
 
 sudo apt-get update && sudo apt-get install -y $packages
 spip-echo "Done installing packages."
-
-if command -v git >/dev/null 2>&1
-then
-    spip-echo "Running git scripts..."
+spip-echo "Running post scripts..."
     
-    for file in post/git*.sh
+for file in post/*.sh
+do
+    sudo chmod +x $file
+    ./$file
+done
+
+spip-echo "Done running post scripts."
+spip-echo "Running $release post scripts..."
+
+if [ -d "post/$release" ]
+then
+    for file in "post/$release"/*.sh
     do
         sudo chmod +x $file
         ./$file
     done
-    
-    spip-echo "Done running git scripts."
-else
-    spip-echo "git is not installed."
 fi
 
-if command -v npm >/dev/null 2>&1
-then
-    spip-echo "Running npm scripts..."
-    
-    for file in post/npm*.sh
-    do
-        sudo chmod +x $file
-        ./$file
-    done
-    
-    spip-echo "Done running npm scripts."
-else
-    spip-echo "npm is not installed."
-fi
-
-if command -v php >/dev/null 2>&1
-then
-    spip-echo "Running php scripts..."
-    
-    for file in post/php*.sh
-    do
-        sudo chmod +x $file
-        ./$file
-    done
-    
-    spip-echo "Done running php scripts."
-else
-    spip-echo "php is not installed."
-fi
-
+spip-echo "Done running $release post scripts."
 spip-echo "Bye bye!"
