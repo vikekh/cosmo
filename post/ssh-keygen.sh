e4c7050479c9a2c74e8ada58a86f55bc5e747027
Viktor Ekholm <vieekk@gmail.com>
2017-04-24 21:52:06 +0200

    Simple structure

--- post/ssh-keygen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. common.sh
-spip-echo "comment="
-read comment
-mkdir ~/.ssh
-ssh-keygen -t rsa -b 4096 -C "$comment" -f ~/.ssh/id_rsa -N ""
--- sources/ubuntu-16.10/dropbox.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# https://www.dropbox.com/en/help/246
-sudo add-apt-repository -y "deb http://linux.dropbox.com/ubuntu xenial main"
-sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
